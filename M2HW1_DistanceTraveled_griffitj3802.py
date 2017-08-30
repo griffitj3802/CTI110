e3a8e93fbf3e4f188946277183a45b5b1ce1596b
griffitj3802 <31459896+griffitj3802@users.noreply.github.com>
2017-08-29 21:40:01 -0400

    Add files via upload

---
+++ M2HW1_DistanceTraveled_griffitj3802.py
@@ -0,0 +1,12 @@
+#CTI-11-
+#M2HW1 - Distance Traveled
+#James Griffith
+#29 August, 2017
+
+speed = 70
+#Distanceafter6
+print( "In 6 hours, the car will travel " + str( ( speed * 6 ) )+ " miles " )
+#DistanceAfter10
+print( "In 10 hours, the car will travel " + str( ( speed * 10 ) )+ " miles " )
+#DistanceAfter15
+print( "In 15 hours, the car will travel " + str( ( speed * 15 ) )+ " miles " )
