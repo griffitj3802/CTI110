b0dc29c49ea0913e3abda040568bd70a1ffe6ca6
griffitj3802 <31459896+griffitj3802@users.noreply.github.com>
2017-12-07 08:51:01 -0500

    Add files via upload

---
+++ M1T1/griffitj3802-M1T1.py
@@ -0,0 +1,21 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ('Hello world')
+Hello world
+>>> 2+2
+4
+>>> 10/3
+3.3333333333333335
+>>> 
+======== RESTART: E:/Fall 2017/CTI 110/Module 1/griffitj3802-M1T1.py ========
+Traceback (most recent call last):
+  File "E:/Fall 2017/CTI 110/Module 1/griffitj3802-M1T1.py", line 5, in <module>
+    pring("Hellow WOrld")
+NameError: name 'pring' is not defined
+>>> 
+======== RESTART: E:/Fall 2017/CTI 110/Module 1/griffitj3802-M1T1.py ========
+Hello World
+>>> 
+======== RESTART: E:/Fall 2017/CTI 110/Module 1/griffitj3802-M1T1.py ========
+Hello World
+>>> 
